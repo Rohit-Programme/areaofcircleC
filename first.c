823efa6dba1a40195531cc7b9c59fbdc887bc5ab
Rohit <rohitkusv@gmail.com>
2024-02-23 12:04:13 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,12 @@
+//calculate area of a circle
+#include<stdio.h>
+int main(){
+    //formula pixrxr
+float pi =3.14;
+int radius;
+printf("input the radius of a cicle");
+scanf("%d",&radius);
+int area = pi*radius*radius;
+printf("the radius of a circle is =%d",area);
+return 0;
+}
